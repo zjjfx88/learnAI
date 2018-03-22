43c445efb353d870d45a669d4a0d679cd02527e0
zjjfx88 <zjj7842806@163.com>
2018-03-22 10:48:47 +0800

    add checkdog and gitignore

---
+++ base/checkdog.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'zhangjingjun'
+__mtime__ = '2018/3/22'
+# ----------Dragon be here!----------
+              ┏━┓      ┏━┓
+            ┏━┛ ┻━━━━━━┛ ┻━━┓
+            ┃       ━       ┃
+            ┃  ━┳━┛   ┗━┳━  ┃
+            ┃       ┻       ┃
+            ┗━━━┓      ┏━━━━┛
+                ┃      ┃神兽保佑
+                ┃      ┃永无BUG！
+                ┃      ┗━━━━━━━━━┓
+                ┃                ┣━┓
+                ┃                ┏━┛
+                ┗━━┓ ┓ ┏━━━┳━┓ ┏━┛
+                   ┃ ┫ ┫   ┃ ┫ ┫
+                   ┗━┻━┛   ┗━┻━┛
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+gold,chihh = 400,400
+
+gold_height = 40 + 10*np.random.randn(gold)
+chihh_height = 25 + 6*np.random.randn(chihh)
+
+plt.hist([gold_height,chihh_height],stacked=True,color=['r','b'])
+
+plt.show()
